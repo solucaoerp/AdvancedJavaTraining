678904942c78d7785dfa408104429ea984e866a2
Charles Borges <solucao.erp@gmail.com>
2023-06-10 15:29:23 -0300

    Produc: atualizando registro 'PUT'

--- dscommerce/src/main/java/com/ibrplanner/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/ibrplanner/dscommerce/controllers/ProductController.java
@@ -43,4 +43,10 @@ public class ProductController {
 
         return ResponseEntity.created(uri).body(dto);
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = productService.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
 }
