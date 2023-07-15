d711a290cf3c83e24cc38eebd572d577744d3fc3
Charles Borges <solucao.erp@gmail.com>
2023-07-15 09:45:50 -0300

    DSCommerce: Adicionando Seguranca - Obtendo o usuario logado

--- dscommerce/src/main/java/com/ibrplanner/dscommerce/services/UserService.java
+++ dscommerce/src/main/java/com/ibrplanner/dscommerce/services/UserService.java
@@ -1,14 +1,19 @@
 package com.ibrplanner.dscommerce.services;
 
+import com.ibrplanner.dscommerce.dtos.UserDTO;
 import com.ibrplanner.dscommerce.entities.Role;
 import com.ibrplanner.dscommerce.entities.User;
 import com.ibrplanner.dscommerce.projections.UserDetailsProjection;
 import com.ibrplanner.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -35,5 +40,23 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+
+            return repository.findByEmail(username).get();
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe(){
+        User user = authenticated();
+        return new UserDTO(user);
     }
 }
