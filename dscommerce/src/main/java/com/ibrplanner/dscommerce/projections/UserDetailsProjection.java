0423ff9e85a18ab946fec1c36b6cfeb4d89918b2
Charles Borges <solucao.erp@gmail.com>
2023-07-14 21:21:48 -0300

    DSCommerce: Adicionando Seguranca - checklist-3.2: UserDetailsService

---
+++ dscommerce/src/main/java/com/ibrplanner/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,12 @@
+package com.ibrplanner.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+
+    String getPassword();
+
+    Long getRoleId();
+
+    String getAuthority();
+}
---
+++ dscommerce/src/main/java/com/ibrplanner/dscommerce/services/UserService.java
@@ -0,0 +1,39 @@
+package com.ibrplanner.dscommerce.services;
+
+import com.ibrplanner.dscommerce.entities.Role;
+import com.ibrplanner.dscommerce.entities.User;
+import com.ibrplanner.dscommerce.projections.UserDetailsProjection;
+import com.ibrplanner.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
