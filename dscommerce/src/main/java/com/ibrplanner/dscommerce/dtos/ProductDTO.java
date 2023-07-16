18b9dcea09f6f7a01c56a41a6b6629608e8ebb6b
Charles Borges <solucao.erp@gmail.com>
2023-07-15 22:59:07 -0300

    DSCommerce: ajustes nas validacoes de dados (refactors)

--- dscommerce/src/main/java/com/ibrplanner/dscommerce/dtos/ProductDTO.java
+++ dscommerce/src/main/java/com/ibrplanner/dscommerce/dtos/ProductDTO.java
@@ -2,10 +2,7 @@ package com.ibrplanner.dscommerce.dtos;
 
 import com.ibrplanner.dscommerce.entities.Category;
 import com.ibrplanner.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +19,7 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido")
     private String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preco deve ser positivo")
     private Double price;
     private String imgUrl;
